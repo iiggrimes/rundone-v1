829f4b2965d6123cecc9a28d7a33a19ac4b81f2e
iiggrimes <iiggrimes@gmail.com>
2025-08-22 10:34:01 -0400

    light start to my first project back

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("New shoe (1) or Recent shoe (2)");
+
+        if (input.nextInt() == 1) {
+
+
+        }
+        else if (input.nextInt() == 2) {
+
+        }
+
+    }
+}
---
+++ src/Shoe.java
@@ -0,0 +1,14 @@
+public class Shoe {
+
+    String shoeBrand;
+    String shoeName;
+    String shoeColor;
+    double shoeSize;
+    int startingMiles;
+    int remainingMiles;
+    int currentMiles;
+    int endOfShoeMiles;
+
+
+
+}
